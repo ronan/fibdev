209c9bb310ee58e21214837aa4c016b76b302aba
Ronan Dowling <ronan@dowlings.me>
2024-04-18 10:09:27 -0500

    Latest tweaks to functionality

--- .devcontainer/backstop.js/backstop.config.js
+++ .devcontainer/backstop.js/backstop.config.js
@@ -5,11 +5,11 @@ module.exports = {
       "width": 1920,
       "height": 1080
     },
-    {
-      "label": "mobile",
-      "width": 360,
-      "height": 800
-    }
+    // {
+    //   "label": "mobile",
+    //   "width": 360,
+    //   "height": 800
+    // }
   ],
   "scenarios": [],
   "paths": {
@@ -19,36 +19,37 @@ module.exports = {
     "bitmaps_test":       "/workspace/outbox/backstop.js/report/bitmaps_test",
     "ci_report":          "/workspace/outbox/backstop.js/report/ci"
   },
-  "report": ["browser"],
+  "report": ["browser","json"],
   "engine": "playwright",
   "onReadyScript": "onReady.js",
   "engineOptions": {
     "browser": "chromium"
   },
   "misMatchThreshold": 0.5,
-  "asyncCaptureLimit": 5,
+  "asyncCaptureLimit": 1,
   "asyncCompareLimit": 10,
   "resembleOutputOptions": {
     "ignoreAntialiasing": true,
     "usePreciseMatching": true
   },
   "debug": false,
-  "debugWindow": false
+  "debugWindow": false,
+  "scenarioLogsInReports": true
 }
 
 try {
-  require('fs').readFileSync('/workspace/inbox/urls.txt', 'utf-8')
-                .split(/\n/)
-                .splice(0, process.env.PAGES_TO_TEST || 100)
-                .forEach((url) => {
-                            module.exports.scenarios.push(
-                              {
-                                "label": url,
-                                "url": `http://static:8009${url}`,
-                                "referenceUrl": `http://drupal${url}`,
-                                "removeSelectors": [".visually-hidden"]
-                              }
-                            );
-                          });
+  require('fs')
+    .readFileSync('/workspace/inbox/urls.txt', 'utf-8')
+    .split(/\n/)
+    .forEach((url) => {
+                module.exports.scenarios.push(
+                  {
+                    "label":          url,
+                    "url":            `https://test-neuroscience-su-d9.pantheonsite.io${url}`,
+                    "referenceUrl":   `https://neuroscience.stanford.edu${url}`
+                  }
+                );
+              });
 } catch (err) {
+  console.log(err);
 }
